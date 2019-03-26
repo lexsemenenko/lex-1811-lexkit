1b5f000169dd0afc722c464627af804d019b31ca
Lex Semenenko <lex@lex.me>
2019-03-25 23:54:53 -0400

    JS: Breakpoints some cleanup

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import './css/main.less'
 // Import a module for its side effects only. This runs module globally
 // without importing any values
 import './js/core/core--events-custom'
-import { _Base } from './js/core/core--base'
 import { _bundle } from './js/core/core--bundles'
-
-// Import Library
-import { _libraryArray } from './js/library/library--array'
 
 // Import Modules
 import './js/modules/module--breakpoints'
@@ -30,56 +26,13 @@ let lexkit
 lexkit = function () {
   $(document).ready(function () { $.event.trigger('on:Ready') })
 
-  bp.setSettings({
-    breakpointsArray: ['small', 'large', 'widescreen']
-  })
-
-  _bundle._add({
-    name: 'Breakpoints Test',
-    event: ['on:Ready', 'on:Resize'],
-    fn: function () {
-      console.log(bp.match('small'), 'small', "down")
-      console.log(bp.match('large'), 'large', "down")
-      console.log(bp.match('widescreen'), 'widescreen', "down")
-      console.log('========================')
-      console.log(bp.match('small', 'up'), 'small', "up")
-      console.log(bp.match('large', 'up'), 'large', "up")
-      console.log(bp.match('widescreen', 'up'), 'widescreen', "up")
-    }
-  })
-
   // =============================================================================
   // Site Specific: Fire Modules
   // =============================================================================
 
-  // _bundle._add({
-  //   name: 'resizeEnd',
-  //   event: ['on:ResizeEnd'],
-  //   fn: function () {
-  //     console.log(this.name)
-  //   }
-  // })
-  // _bundle._add({
-  //   name: 'Ready',
-  //   event: ['on:Ready'],
-  //   fn: function () {
-  //     console.log(this.name)
-  //   }
-  // })
-  // _bundle._add({
-  //   name: 'Resize',
-  //   event: ['on:Resize'],
-  //   fn: function () {
-  //     console.log(this.name)
-  //   }
-  // })
-  // _bundle._add({
-  //   name: 'Scroll',
-  //   event: ['on:Scroll'],
-  //   fn: function () {
-  //     console.log(this.name)
-  //   }
-  // })
+  bp.setSettings({
+    breakpointsArray: ['small', 'large', 'widescreens']
+  })
 
   collapsibles({
     block: '.menu-mobile',
--- src/js/modules/module--breakpoints.js
+++ src/js/modules/module--breakpoints.js
@@ -1,22 +1,22 @@
-//= =============================================================================
+// ==============================================================================
 // Module: Breakpoints
 // ==============================================================================
 
-let bp = (function () {
-
+export let bp = (function () {
   let options = {
     elPassing: 'body',
     breakpointsArray: null
   }
-
   let s
+  let currentBreakpoint
+
   function _mergeSettings (optionsPassed) {
     s = Object.assign({}, options, optionsPassed)
-    console.log(s)
   }
 
   function _getCurrentBreakpoint () {
-    var currentBreakpoint = window.getComputedStyle($(s.elPassing)[0], ':before')
+    currentBreakpoint = window
+      .getComputedStyle($(s.elPassing)[0], ':before')
       .getPropertyValue('content')
       .replace(/\"/g, '') // remove quotes
     return currentBreakpoint
@@ -26,12 +26,10 @@ let bp = (function () {
     let bpArray = s.breakpointsArray
     let passed = bpArray.indexOf(breakpoint)
     let current = bpArray.indexOf(_getCurrentBreakpoint())
-
     if (direction === 'up') return current > passed
     else return current <= passed // Downwards is default
   }
 
-  // Public
   return {
     match: function (bp,  dir) {
       return _directons(bp,  dir)
@@ -44,5 +42,3 @@ let bp = (function () {
     }
   }
 })()
-
-export { bp }
